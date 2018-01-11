58a62812dc48bea535dd6ed60ab97faaba4061d3
AliiiceW <aliceklwilliams@gmail.com>
2018-01-11 20:59:28 +0000

    Add Options for Work and Break Durations

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-const workTime = 0.1;
-const shortBreakTime = 0.1;
-const longBreakTime = 0.1;
 const cycles = 4;
 
-const timeDisplay = document.querySelector("#time-left");
-const periodDisplay = document.querySelector("#period");
+const timeDisplay = document.querySelector("#time__text");
+const periodDisplay = document.querySelector("#period__text");
 const startBttn = document.querySelector("#start");
+const options = document.querySelectorAll(".time-option");
 
-let currSecs = 0;
+let pomodoroOptions = {
+    work: 0,
+    short: 0,
+    long: 0
+}
+
+let currSecs;
 let timer;
 let isWorking = false;
 let workCounter = 0;
 
-function startPomodoro(){
+function startPomodoro() {
 
-    setDisplay("Working", workTime);
+    clearTimeout(timer);
+
+    currSecs = pomodoroOptions.work;
+
+    setDisplay("Working", currSecs);
     isWorking = true;
 
     timer = setTimeout(updatePomodoro, 1000);
 }
 
-function updatePomodoro(){
+function updatePomodoro() {
     currSecs--;
     timeDisplay.textContent = getDisplayTime(currSecs);
 
-    if(currSecs === -1){ //Reached End of current time period
+    if (currSecs === -1) { //Reached End of current time period
 
-        if(isWorking){
+        if (isWorking) {
             workCounter++;
-            if(workCounter === cycles){
-                setDisplay("Long Break", longBreakTime);
+            if (workCounter === cycles) {
+                setDisplay("Long Break", pomodoroOptions.long);
                 workCounter = 0;
-            } else{
-                setDisplay("Short Break", shortBreakTime);
-            }  
-        }
-        else{
-            setDisplay("Working", workTime);
+            } else {
+                setDisplay("Short Break", pomodoroOptions.short);
+            }
+        } else {
+            setDisplay("Working", pomodoroOptions.work);
         }
         isWorking = !isWorking;
     }
@@ -44,30 +51,42 @@ function updatePomodoro(){
     timer = setTimeout(updatePomodoro, 1000);
 }
 
-function setDisplay(periodName, time){
+function setDisplay(periodName, time) {
     periodDisplay.textContent = periodName;
-    currSecs = calculateSecs(time);
+    currSecs = time;
     timeDisplay.textContent = getDisplayTime(currSecs);
 }
 
-function calculateSecs(minutes){
-    return minutes*60;
+function calculateSecs(minutes) {
+    return minutes * 60;
 }
 
-function formatZero(time){
-    if(time <= 9 && time >= 0){
-        return "0"+time;
-    } else{
+function formatZero(time) {
+    if (time <= 9 && time >= 0) {
+        return "0" + time;
+    } else {
         return time;
     }
 }
 
-function getDisplayTime(totalSeconds){
-    const minutes = formatZero(Math.floor(totalSeconds/60));
-    const seconds = formatZero(totalSeconds%60);
+function getDisplayTime(totalSeconds) {
+
+    const minutes = formatZero(Math.floor(totalSeconds / 60));
+    const seconds = formatZero(totalSeconds % 60);
 
     return `${minutes}:${seconds}`;
 }
 
-startBttn.addEventListener("click", startPomodoro);
+function changeTime(e) {
+    const change = parseInt(e.target.dataset.change);
+    if (change) {
 
+        const newMins = parseInt(this.querySelector(".option__text").textContent) + change;
+        this.querySelector(".option__text").textContent = formatZero(newMins);
+
+        pomodoroOptions[this.dataset.option] = calculateSecs(newMins);
+    }
+}
+
+startBttn.addEventListener("click", startPomodoro);
+options.forEach(option => option.addEventListener("click", changeTime));
